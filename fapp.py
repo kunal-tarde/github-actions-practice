7ba1a47f79113cd25a63dbff1f6d6b16943349e9
Kunal Tarde <tardekunal99@gmail.com>
2026-08-09 12:37:50 +0530

    Update fapp.py

--- fapp.py
+++ fapp.py
@@ -1,5 +1,5 @@
 # this code is from https://github.com/kunal-tarde/github-actions-practice/edit/main/fapp.py
-# this code is for kunal
+
 # Program to display the Fibonacci sequence up to n-th term
 
 nterms = int(input("How many terms? "))
